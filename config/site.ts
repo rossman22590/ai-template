8a6b9eb7a047fca6a9e6662bbd7dcb74a0109f0e
Ross Cohen <rosscohen1990@gmail.com>
2023-05-10 12:43:27 -0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,18 +4,10 @@ interface SiteConfig {
   name: string
   description: string
   mainNav: NavItem[]
-  links: {
-    twitter: string
-    github: string
-  }
 }
 
 export const siteConfig: SiteConfig = {
-  name: "Mercury",
+  name: "Info Scan",
   description: "Unlock the secrets of any website",
   mainNav: [],
-  links: {
-    twitter: "https://twitter.com/nolansym",
-    github: "https://github.com/Jordan-Gilliam/ai-template",
-  },
 }
