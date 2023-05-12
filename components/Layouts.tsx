bdcca579bcbb517ca5cd68e6a8bb15dedcfbff3d
Ross Cohen <rosscohen1990@gmail.com>
2023-05-12 15:47:02 -0400

    Update Layouts.tsx

--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -94,7 +94,7 @@ function BackgroundColorBlur() {
       aria-hidden="true"
     >
       <div
-        className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#46ffdd] to-[#776fff] opacity-25"
+        className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#6f42c1] to-[#6f42c1] opacity-25"
         style={{
           clipPath:
             "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
